79f285aa15a68a74ddd19210dbd0d3142422db1e
David Guerrero <daviguer890@gmail.com>
2025-02-26 18:07:43 -0800

    test

--- index.py
+++ index.py
@@ -1,15 +1,22 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 from time import sleep
+import threading
 import requests
-    
+
 PORT = 8000
 
 def consultUser(id):
-    requests.get(f'https://api-ticktick.onrender.com/api/v1/users/{id}/')
-    print('Activated API')
-
-with HTTPServer(('0.0.0.0', PORT), SimpleHTTPRequestHandler) as httpd:
-    httpd.serve_forever()
     while True:
-        consultUser(1)
-        sleep(780) # 13 minutes
+        requests.get(f'https://api-ticktick.onrender.com/api/v1/users/{id}/')
+        print('Activated API')
+        sleep(780)  # 13 minutos
+
+# Configurar y ejecutar el servidor en un hilo separado
+server = HTTPServer(('0.0.0.0', PORT), SimpleHTTPRequestHandler)
+
+# Iniciar el proceso de consulta en un hilo aparte
+thread = threading.Thread(target=consultUser, args=(1,), daemon=True)
+thread.start()
+
+print(f"Starting server on port {PORT}...")
+server.serve_forever()
