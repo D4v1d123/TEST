19d1c1ff85ad4f9e1cc8c60d201ae94f512d2618
David Guerrero <daviguer890@gmail.com>
2025-02-26 17:17:28 -0800

    test

---
+++ index.py
@@ -0,0 +1,10 @@
+from time import sleep
+import requests
+
+def consultUser(id):
+    requests.get(f'https://api-ticktick.onrender.com/api/v1/users/{id}/')
+    print('Activated API')
+    
+while True:
+    consultUser(1)
+    sleep(780) # 13 minutes
