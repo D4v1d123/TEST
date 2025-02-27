7f586e4480a60fe3885c9e3ec43bd5bd78efa2ad
David Guerrero <daviguer890@gmail.com>
2025-02-26 17:31:27 -0800

    test

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 from time import sleep
 import requests
+    
+PORT = 8000
 
 def consultUser(id):
     requests.get(f'https://api-ticktick.onrender.com/api/v1/users/{id}/')
     print('Activated API')
-    
-while True:
-    consultUser(1)
-    sleep(780) # 13 minutes
+
+with HTTPServer(('0.0.0.0', PORT), SimpleHTTPRequestHandler) as httpd:
+    while True:
+        consultUser(1)
+        sleep(780) # 13 minutes
+        httpd.serve_forever()
