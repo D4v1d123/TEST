6dff62336e6797f589eeac4466ed21be6e5edd3a
David Guerrero <daviguer890@gmail.com>
2025-02-26 17:44:40 -0800

    test

--- index.py
+++ index.py
@@ -8,8 +8,8 @@ def consultUser(id):
     requests.get(f'https://api-ticktick.onrender.com/api/v1/users/{id}/')
     print('Activated API')
 
-with HTTPServer(('https://test-sbgp.onrender.com', PORT), SimpleHTTPRequestHandler) as httpd:
+with HTTPServer(('0.0.0.0', PORT), SimpleHTTPRequestHandler) as httpd:
+    httpd.serve_forever()
     while True:
         consultUser(1)
         sleep(780) # 13 minutes
-        httpd.serve_forever()
